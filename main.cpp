ac64bb8e88506cb50e6b768e57d82b0a5232616e
Kevin Masterson <cybermind@gmail.com>
2024-06-16 23:06:38 -0400

    update license, remove cvsignore

--- main.cpp
+++ main.cpp
@@ -1,30 +1,13 @@
 /*
-
-RocketMod - Rocketlaunchers-Only Plugin
-Copyright QMM Team 2005
-http://www.q3mm.org/
-
-Licensing:
-    QMM is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
-
-    QMM is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with QMM; if not, write to the Free Software
-    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+RocketMod - Rocket Launchers-Only Plugin
+Copyright 2004-2024
+https://github.com/thecybermind/rocketmod_qmm/
+3-clause BSD license: https://opensource.org/license/bsd-3-clause
 
 Created By:
-    Kevin Masterson a.k.a. CyberMind <cybermind@users.sourceforge.net>
+    Kevin Masterson < cybermind@gmail.com >
 
 */
-
-/* $Id: main.cpp,v 1.4 2006/01/30 00:41:42 cybermind Exp $ */
 
 #include "version.h"
 #include <q_shared.h>
--- version.h
+++ version.h
@@ -1,30 +1,13 @@
 /*
-
-RocketMod - Rocketlaunchers-Only Plugin
-Copyright QMM Team 2005
-http://www.q3mm.org/
-
-Licensing:
-    QMM is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
-
-    QMM is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with QMM; if not, write to the Free Software
-    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+RocketMod - Rocket Launchers-Only Plugin
+Copyright 2004-2024
+https://github.com/thecybermind/rocketmod_qmm/
+3-clause BSD license: https://opensource.org/license/bsd-3-clause
 
 Created By:
-    Kevin Masterson a.k.a. CyberMind <cybermind@users.sourceforge.net>
+    Kevin Masterson < cybermind@gmail.com >
 
 */
-
-/* $Id: version.h,v 1.4 2006/01/30 00:41:42 cybermind Exp $ */
 
 #ifndef __VERSION_H__
 #define __VERSION_H__
@@ -32,6 +15,6 @@ Created By:
 #define ROCKETMOD_QMM_VERSION			"1.1.0"
 #define ROCKETMOD_QMM_VERSION_DWORD		1,1,0,0
 #define ROCKETMOD_QMM_COMPILE			__TIME__ " " __DATE__
-#define ROCKETMOD_QMM_BUILDER			"CyberMind"
+#define ROCKETMOD_QMM_BUILDER			"cybermind"
 
 #endif //__VERSION_H__
