ab98d9e0d6b0a4a79e3d75d7f9e83f998ae41afb
Kevin Masterson <cybermind@gmail.com>
2025-05-22 00:11:29 -0400

    update build scripts from stub_qmm

--- .github/build/windows/debug.bat
+++ .github/build/windows/debug.bat
@@ -1,2 +1,6 @@
-msbuild .\msvc\rocketmod_qmm.vcxproj /p:Configuration=Debug-Q3A /p:Platform=x86
-msbuild .\msvc\rocketmod_qmm.vcxproj /p:Configuration=Debug-Q3A /p:Platform=x64
+for %%x in (
+        Q3A
+       ) do (
+         msbuild .\msvc\rocketmod_qmm.vcxproj /p:Configuration=Debug-%%x /p:Platform=x86
+         msbuild .\msvc\rocketmod_qmm.vcxproj /p:Configuration=Debug-%%x /p:Platform=x64
+       )
--- .github/build/windows/release.bat
+++ .github/build/windows/release.bat
@@ -1,3 +1,6 @@
-msbuild .\msvc\rocketmod_qmm.vcxproj /p:Configuration=Release-Q3A /p:Platform=x86
-msbuild .\msvc\rocketmod_qmm.vcxproj /p:Configuration=Release-Q3A /p:Platform=x64
-
+for %%x in (
+        Q3A
+       ) do (
+         msbuild .\msvc\rocketmod_qmm.vcxproj /p:Configuration=Release-%%x /p:Platform=x86
+         msbuild .\msvc\rocketmod_qmm.vcxproj /p:Configuration=Release-%%x /p:Platform=x64
+       )
