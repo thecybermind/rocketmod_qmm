23b260a9fea719ff3104da419269f74901afc767
Kevin Masterson <k.m.masterson@gmail.com>
2026-03-29 19:41:56 +0300

    changes with qmm 2.6.0 and pifv 4:4

--- src/main.cpp
+++ src/main.cpp
@@ -66,7 +66,7 @@ C_DLLEXPORT void QMM_Detach() {
 C_DLLEXPORT intptr_t QMM_vmMain(intptr_t cmd, intptr_t* args) {
 	if (cmd == GAME_INIT) {
 		// init msg
-		QMM_WRITEQMMLOG("RocketMod v" ROCKETMOD_QMM_VERSION " by " ROCKETMOD_QMM_BUILDER " is loaded\n", QMMLOG_INFO);
+		QMM_WRITEQMMLOG(QMMLOG_INFO, "RocketMod v" ROCKETMOD_QMM_VERSION " by " ROCKETMOD_QMM_BUILDER " is loaded\n");
 
 		// register cvars
 		g_syscall(G_CVAR_REGISTER, nullptr, "rocketmod_version", ROCKETMOD_QMM_VERSION, CVAR_ROM | CVAR_SERVERINFO);
